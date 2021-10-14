278e16f11c5dd28b6f0fc2f303fdb9078309aac5
Enoch2k2 <enoch2k2@hotmail.com>
2021-10-14 10:46:39 -0500

    all of the test that passed won't run

---
+++ src/components/incrementButton/IncrementButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const incrementButton = () => {
+  return (
+    <div>
+      <h1>Increment The Counter</h1>
+    </div>
+  )
+}
+
+export default incrementButton
---
+++ src/components/incrementButton/_test_/IncrementButton.test.js
@@ -0,0 +1,36 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import IncrementButton from '../IncrementButton';
+
+test('has a header of Increment The Counter', () => {
+  render(<IncrementButton />);
+
+  const header = screen.getByRole('heading', { name: 'Increment The Counter'});
+
+  expect(header).toBeInTheDocument();
+})
+
+// describe('incrementor button', () => {
+test('has a button with the name of Click Me!', () => {
+  render(<IncrementButton />)
+
+  const button = screen.getByRole('button', { name: 'Click Me!' });
+  
+  expect(button).toBeInTheDocument();
+})
+// })
+
+test('has a counter that can increment', () => {
+  render(<IncrementButton />);
+  
+  const button = screen.getByRole('button', { name: 'Click Me!' });
+  const count = screen.getByText('0');
+
+  // click the button
+  fireEvent(button, 'click')
+  // expect the count to be 1
+  expect(count).toBe('1');
+  // click the button again
+  fireEvent(button, 'click');
+  // expect the count to be 2
+  expect(count).toBe('2');
+})
